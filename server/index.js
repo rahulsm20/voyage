84ad9d463d1522cc33bd771069dae6c12bafb5b9
Sakshi Singh <sakshiss2324@gmail.com>
2022-12-27 00:36:40 +0530

    display of dp while logging via google

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use('/users',userRoutes)
-const CONNECTION_URL = (process.env.MONGO_KEY) //Insert mongoDB key here
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = "mongodb+srv://sakshiss2324:Phoenix2324@cluster0.xa2t2ru.mongodb.net/?retryWrites=true&w=majority" //Insert mongoDB key here
+const PORT = process.env.PORT|| 5500;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
